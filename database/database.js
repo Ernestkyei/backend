62e72e255325e1d498a5d700c8956f5c57641fe1
ErnestKyei <kyeiernest86@gmail.com>
2026-09-04 05:46:44 +0000

    admin table

--- database/database.js
+++ database/database.js
@@ -4,7 +4,10 @@ import "dotenv/config";
 const { Pool } = pg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
